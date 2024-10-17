ee788e580a32c89306f8318616e34456f52671e3
diovalda <ferrenxx@gmail.com>
2024-10-17 09:00:45 +0700

    feat: get presensi

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::resource('siswa', SiswaController::class);
     Route::resource('guru', GuruController::class);
     Route::post('presensi', [PresensiController::class, 'presensi']);
+    Route::get('presensi/telat', [PresensiController::class, 'getTelat']);
+    Route::get('presensi/hadir', [PresensiController::class, 'getHadir']);
+    Route::get('presensi/minggu', [PresensiController::class, 'getPresensiWeekly']);
+    Route::get('presensi/bulan', [PresensiController::class, 'getPresensiMonthly']);
     Route::post('auth/logout', [AuthController::class, 'logout']);
 });
